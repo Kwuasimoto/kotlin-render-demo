37fca071ff864e6032be400fef13bccb16eb4cbf
Thomas Shank <200346862@student.georgianc.on.ca>
2023-08-01 10:39:30 -0400

    Added post mapping, testing communication between render ui and render server in different web services.

--- src/main/kotlin/org/kwuasimoto/renderplaindemo/rest/contollers/TestController.kt
+++ src/main/kotlin/org/kwuasimoto/renderplaindemo/rest/contollers/TestController.kt
@@ -1,18 +1,37 @@
 package org.kwuasimoto.renderplaindemo.rest.contollers
 
+import jakarta.servlet.http.HttpServletRequest
+import org.slf4j.LoggerFactory
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
+data class DataRequest(
+    val payload: String
+)
 
 @RestController
 @RequestMapping("/test")
 class TestController {
 
+    private val logger = LoggerFactory.getLogger(TestController::class.java)
+
+
+
     @GetMapping
-    fun helloWorld(): ResponseEntity<String> {
+    fun helloWorld(request: HttpServletRequest): ResponseEntity<String> {
+        logger.info("Incoming fetch request")
+        logger.info("$request")
         return ResponseEntity.ok("Hello World")
     }
 
+    @PostMapping
+    fun postData(@RequestBody incoming: DataRequest, request: HttpServletRequest): ResponseEntity<*> {
+        logger.info("Incoming post request")
+        logger.info("$request")
+        return ResponseEntity.ok(incoming)
+    }
 }
